509fb5eecc10260ffeb942a3a2f1b2d2254f0aa8
jsifantu@comcast.net <jsifantu@comcast.net>
2018-06-07 16:42:43 -0400

    Adding new SQL script.

---
+++ Scripts/FinanceScript.sql
@@ -0,0 +1,24 @@
+﻿/**
+ * Object:  Table [dbo].[JdsFinanceTransactions]    
+ * Script Date: 9/29/2017
+ * Jean-Do Sifantus
+ **/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].JdsFinanceTransactions(
+	[Uid] [uniqueidentifier] NOT NULL,
+	[Description] [nvarchar](256) NOT NULL,
+	[Amount] [float] NOT NULL,
+	[Category] [nvarchar](256) NOT NULL,
+	[TransactionDate] [datetime2](0) NOT NULL,
+	[AccountNum] [uniqueidentifier] NOT NULL,
+ CONSTRAINT [PK_JdsFinanceTransactions] PRIMARY KEY CLUSTERED 
+ (
+	[Uid] ASC
+ )
+)
+
+GO
+
